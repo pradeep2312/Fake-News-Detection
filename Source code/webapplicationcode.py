0955f04f62e9e21e2688cfd9d3fa63300181d581
pradeep2312 <saikukunooru@gmail.com>
2023-05-07 10:52:41 -0500

    Source code and Dataset

---
+++ Source code/webapplicationcode.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import pickle
+import re
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Embedding, LSTM
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
+
+port_stem = PorterStemmer()
+vectorization = TfidfVectorizer()
+
+vector_form=pickle.load(open('GB.pk1','rb'))
+load_model=pickle.load(open('Sequential.pkl','rb'))
+
+
+
+def predictData(sourceData):
+  sourceData = tokenizer.texts_to_sequences(sourceData)
+  sourceData = pad_sequences(sourceData, maxlen=maxlen)
+  final_value = (load_model.predict(sourceData)>=0.5).astype(int)
+  return final_value
+
+
+
+if __name__ == '__main__':
+    st.title('Fake News Detection  ')
+    st.subheader("Give the input News below")
+    sentence = st.text_area("Enter your news content here", "",height=200)
+    predict_btt = st.button("predict")
+    if predict_btt:
+        prediction_class=predictData(sentence)
+        print(prediction_class)
+        if prediction_class == [0]:
+            st.success('Fake')
+        if prediction_class == [1]:
+            st.warning('Real')
